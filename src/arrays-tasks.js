37dd78d533d56412aaa5cb307dc462a102ce3086
koonukaame <marialezhebokova@yandex.ru>
2024-12-04 13:37:54 +0300

    feat: solve findAllOccurrences

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -79,8 +79,10 @@ function findElement(arr, value) {
  *    findAllOccurrences([ null, undefined, null ], null) => 2
  *    findAllOccurrences([ true, 0, 1, 'true' ], true) => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  return arr.reduce((acc, curr) => {
+    return curr === item ? acc + 1 : acc;
+  }, 0);
 }
 
 /**

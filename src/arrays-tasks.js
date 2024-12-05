e4b0c39c882fc7783e409c5f4d71074a1f044d23
koonukaame <marialezhebokova@yandex.ru>
2024-12-05 13:45:33 +0300

    feat: solve calculateBalance

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -326,8 +326,9 @@ function selectMany(arr, childrenSelector) {
  *   calculateBalance([ [ 10, 8 ], [ 1, 5 ] ])  => (10 - 8) + (1 - 5) = 2 + -4 = -2
  *   calculateBalance([]) => 0
  */
-function calculateBalance(/* arr */) {
-  throw new Error('Not implemented');
+function calculateBalance(arr) {
+  return arr.reduce((acc, [income, expenses]) => acc + (income - expenses), 0);
+  // [income, expenses] - текущий обрабатываемый элемент массива
 }
 
 /**

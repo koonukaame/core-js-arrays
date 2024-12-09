c4b4dd7bcaa13ef971e76b34be31c1bb986b24ed
koonukaame <marialezhebokova@yandex.ru>
2024-12-09 15:09:52 +0300

    feat: solve getIdentityMatrix

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -418,8 +418,14 @@ function getFalsyValuesCount(arr) {
  *                              [0,0,0,1,0],
  *                              [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  return Array.from({ length: n })
+    .fill()
+    .map((_, i) =>
+      Array.from({ length: n })
+        .fill(0)
+        .map((__, j) => (i === j ? 1 : 0))
+    );
 }
 
 /**
